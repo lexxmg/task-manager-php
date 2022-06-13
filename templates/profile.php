a2edd64554c8286bdb39d1db37d4097e1ce63e3a
mac-book <lexx.mg@gmail.com>
2022-06-13 15:33:08 +0300

    sync

--- templates/profile.php
+++ templates/profile.php
@@ -1,11 +1,27 @@
 <main class="profile">
   <h1 class="profile__title"><?=getTitle($menu)?></h1>
 
-  <pre>
-      <?php var_dump($authUser)?>
-  </pre>
+  <p class="profile__text">Данные пользователя:</p>
 
-  <pre>
-      <?php var_dump($userGroups)?>
-  </pre>
+  <ul class="profile__list profile-list">
+      <li class="profile-list__item">
+          <span class="profile-list__text">Ф. И. О.</span><?=$authUser['name']?>
+      </li>
+      <li class="profile-list__item">
+          <span class="profile-list__text">email:</span><?=$authUser['email']?>
+      </li>
+      <li class="profile-list__item">
+          <span class="profile-list__text">Телефон:</span><?=$authUser['tel']?>
+      </li>
+  </ul>
+
+  <p class="profile__text">Группы пользователя:</p>
+
+  <ul class="profile__list profile-list">
+      <?php foreach ($userGroups as $key => $group): ?>
+          <li class="profile-list__item">
+              <span class="profile-list__text"><?=$group['name']?></span>(<?=$group['description']?>)
+          </li>
+      <?php endforeach; ?>
+  </ul>
 </main>
